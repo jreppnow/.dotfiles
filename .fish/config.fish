c80dee4348d495fdd52b1173e0261f2a1b8c6f2a
Janosch Reppnow <janoschre@gmail.com>
2021-11-08 15:19:11 +0100

    update fish config

--- .fish/config.fish
+++ .fish/config.fish
@@ -1,1 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set CMAKE_EXPORT_COMPILE_COMMANDS TRUE
+
+# see https://github.com/ivakyb/fish_ssh_agent
+fish_ssh_agent
+
 set -x PATH $PATH ~/.cargo/bin/ /snap/bin/
