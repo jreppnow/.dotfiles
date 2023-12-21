251cfb6c1d67f674a824670b9bd87f60dd8b2604
ヤーノシュ <janosch.reppnow@pol.co.jp>
2023-12-21 17:54:05 +0900

    add grepping for selected text

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,6 +2,24 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>sl', builtin.live_grep, {})
+vim.keymap.set('v', '<leader>sl', function()
+    local current_visual_selection = function()
+        -- thanks to https://github.com/nvim-telescope/telescope.nvim/issues/1923#issuecomment-1122642431
+        vim.cmd('noau normal! "vy"')
+        local text = vim.fn.getreg('v')
+        vim.fn.setreg('v', {})
+
+        text = string.gsub(text, "\n", "")
+        if #text > 0 then
+            return text
+        else
+            return ''
+        end
+    end
+    builtin.grep_string({
+        default_text = current_visual_selection()
+    })
+end, {})
 vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>sa', builtin.builtin, {})
 vim.keymap.set('n', '<leader>sd', builtin.lsp_definitions, {})
