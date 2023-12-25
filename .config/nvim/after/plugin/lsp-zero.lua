7ec69e00fd56fdb5ccca9ac0290e0b84520e40f0
ヤーノシュ <janosch.reppnow@pol.co.jp>
2023-12-25 19:01:33 +0900

    update keybinds for searching errors in current file

--- .config/nvim/after/plugin/lsp-zero.lua
+++ .config/nvim/after/plugin/lsp-zero.lua
@@ -49,8 +49,8 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(_, bufnr)
-    print("LSP attached!")
+local function set_keybinds(_, bufnr)
+    print("LSP attached, setting keybinds..!")
 
     local opts = { buffer = bufnr, remap = false }
 
@@ -58,13 +58,16 @@ lsp.on_attach(function(_, bufnr)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>ge", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "<leader>gpe", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "<leader>ra", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>sr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
+end
+
+
+lsp.on_attach(set_keybinds)
 
 lsp.format_on_save({
     format_opts = {
